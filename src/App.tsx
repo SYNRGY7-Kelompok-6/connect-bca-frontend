94d75099af3ef42eae2841dd9668219e6c308373
Calvin <calvinalvito19@gmail.com>
2024-08-01 15:49:57 +0700

    feature: Add Loading Context and Preload

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Login from "./pages/login";
 import UnderMaintenance from "./pages/UnderMaintenance";
 import { AuthProvider } from "./contexts/AuthContext";
 import { BankStatementProvider } from "./contexts/BankStatementContext";
+import { LoadingProvider } from "./contexts/LoadingContext";
 import Beranda from "./pages/Beranda";
 import PrivateRoute from "./routes/PrivateRoutes";
 
@@ -13,18 +14,20 @@ function App() {
     <Router>
       <AuthProvider>
         <BankStatementProvider>
-          <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/maintenance" element={<UnderMaintenance />} />
-            <Route
-              path="/"
-              element={
-                <PrivateRoute>
-                  <Beranda />
-                </PrivateRoute>
-              }
-            />
-          </Routes>
+          <LoadingProvider>
+            <Routes>
+              <Route path="/login" element={<Login />} />
+              <Route path="/maintenance" element={<UnderMaintenance />} />
+              <Route
+                path="/"
+                element={
+                  <PrivateRoute>
+                    <Beranda />
+                  </PrivateRoute>
+                }
+              />
+            </Routes>
+          </LoadingProvider>
         </BankStatementProvider>
       </AuthProvider>
     </Router>
