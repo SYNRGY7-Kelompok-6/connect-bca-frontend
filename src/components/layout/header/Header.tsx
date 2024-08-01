1a1f5a37294690b8f6cd955e14a61451850ff7f9
Calvin <calvinalvito19@gmail.com>
2024-08-01 15:51:32 +0700

    fix: fixed component structure

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -8,16 +8,14 @@ const Header: React.FC = () => {
           <div className="flex items-center gap-2">
             <img src="/LogoBCA.png" alt="Logo Connect by BCA" />
             <div className="flex flex-col gap-1.5">
-              <h1 className="font-semibold text-white text-lg">Connect</h1>
+              <h1 className="font-semibold text-white text-lg" aria-label="Connect">Connect</h1>
               <p className="text-white text-sm">by BCA</p>
             </div>
           </div>
-          <div className="font-semibold text-lg text-white">
-            <h2>Internet Banking</h2>
-          </div>
+          <h2 className="font-semibold text-lg text-white" aria-label="Internet Banking">Internet Banking</h2>
         </div>
       </div>
-      <div className="bg-white h-6 w-full"></div>
+      <div className="bg-primary-blue h-3 w-full"></div>
     </>
   );
 };
