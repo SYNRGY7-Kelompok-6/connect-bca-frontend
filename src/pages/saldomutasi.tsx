1e57c619e4175dee121be8715f4b7abcc3bf4b40
Calvin <calvinalvito19@gmail.com>
2024-08-03 21:37:33 +0700

    feat: add preloading and fetch data functionality in SaldoMutasi component

--- src/pages/saldomutasi.tsx
+++ src/pages/saldomutasi.tsx
@@ -1,22 +1,59 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import Header from "../components/layout/header";
 import InfoUser from "../components/layout/infouser";
 import MenuFitur from "../components/layout/menufitur";
-import Dropdown from "../components/layout/dropdown";
-
-// import MutasiRekening from "./MutasiRekening";
+import Dropdown from "../components/layout/dropdown/DropDown";
+import { useLoading } from "../contexts/useLoading";
+import { useAuth } from "../contexts/useAuth";
+import useBankStatement from "../contexts/useBankStatement";
+import MutasiRekening from "./MutasiRekening";
 import InfoSaldo from "./InfoSaldo";
+import Preloading from "../components/base/preloading/preloading";
 
 const SaldoMutasi: React.FC = () => {
   const location = useLocation();
+  const { loading, setLoading } = useLoading();
+  const { fetchLoginInfo } = useAuth();
+  const { fetchBankStatement, fetchAccountMonthly } =
+    useBankStatement();
+
+  const [hasFetchedData, setHasFetchedData] = useState(false);
+
+  useEffect(() => {
+    if (!hasFetchedData) {
+      const fetchData = async () => {
+        setLoading(true);
+        try {
+          await Promise.all([
+            fetchLoginInfo(),
+            fetchBankStatement(),
+            fetchAccountMonthly(8),
+          ]);
+          setHasFetchedData(true);
+        } catch (err) {
+          console.error("Error fetching data", err);
+        } finally {
+          setLoading(false);
+        }
+      };
+
+      fetchData();
+    }
+  }, [
+    hasFetchedData,
+    fetchLoginInfo,
+    fetchBankStatement,
+    fetchAccountMonthly,
+    setLoading,
+  ]);
 
   const renderContent = () => {
     switch (location.pathname) {
       case "/saldo-mutasi/informasi-saldo-rekening":
         return <InfoSaldo />;
       case "/saldo-mutasi/mutasi-rekening":
-        return <InfoSaldo />;
+        return <MutasiRekening />;
       default:
         return <InfoSaldo />;
     }
@@ -24,28 +61,34 @@ const SaldoMutasi: React.FC = () => {
 
   return (
     <div className="bg-primary-dark-blue font-sans">
-      <Header />
-      <InfoUser />
-      <MenuFitur />
-      <section className="container mx-auto mt-[50px] pb-[50px]">
-        <div className="flex flex-row gap-[80px]">
-          <Dropdown
-            buttonLabel="Informasi Saldo & Mutasi"
-            activeItem={location.pathname}
-            items={[
-              {
-                label: "Informasi Saldo Rekening",
-                href: "/saldo-mutasi/informasi-saldo-rekening",
-              },
-              {
-                label: "Mutasi Rekening",
-                href: "/saldo-mutasi/mutasi-rekening",
-              },
-            ]}
-          />
-          {renderContent()}
-        </div>
-      </section>
+      {loading ? (
+        <Preloading />
+      ) : (
+        <>
+          <Header />
+          <InfoUser />
+          <MenuFitur />
+          <section className="container mx-auto mt-[50px] pb-[50px]">
+            <div className="flex flex-row gap-[80px]">
+              <Dropdown
+                buttonLabel="Informasi Saldo & Mutasi"
+                activeItem={location.pathname}
+                items={[
+                  {
+                    label: "Informasi Saldo Rekening",
+                    href: "/saldo-mutasi/informasi-saldo-rekening",
+                  },
+                  {
+                    label: "Mutasi Rekening",
+                    href: "/saldo-mutasi/mutasi-rekening",
+                  },
+                ]}
+              />
+              {renderContent()}
+            </div>
+          </section>
+        </>
+      )}
     </div>
   );
 };
