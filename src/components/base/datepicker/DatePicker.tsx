22691fb5284189d99bdd71578fa9916e91561fea
unknown <erdonauz@gmail.com>
2024-08-06 10:03:59 +0800

    fix: input popup fix

--- src/components/base/datepicker/DatePicker.tsx
+++ src/components/base/datepicker/DatePicker.tsx
@@ -32,9 +32,10 @@ const DatePicker: React.FC<PopupProps> = ({
     <div
       className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50"
       aria-label={labelPopup}
+      aria-modal="true"
     >
       <div className={`bg-primary-light-blue items-center text-center flex flex-col justify-end text-primary-dark-blue w-max ${className}`}>
-        <h1 className="">
+        <h1 aria-label="">
           {message}
         </h1>
         {
@@ -43,6 +44,7 @@ const DatePicker: React.FC<PopupProps> = ({
         <Calendar onChange={setValue} value={value} className='w-full border-none !font-sans' />
         <div className="w-full flex gap-1 p-[18px] justify-end">
           <button
+            aria-label="tombol keluar popup"
             type="button"
             onClick={handleClose}
             className="flex gap-4 bg-primary-light-blue px-[36.5px] py-[10px] rounded-xl border border-primary-dark-blue text-primary-dark-blue font-semibold hover:bg-gray-300"
@@ -50,6 +52,7 @@ const DatePicker: React.FC<PopupProps> = ({
               Batal
           </button>
           <button
+            aria-label="tombol submit popup"
             type="button"
             onClick={() => handleSubmit(value)}
             className="flex gap-4 bg-primary-blue px-[36.5px] py-[10px] rounded-xl text-white font-semibold hover:bg-primary-dark-blue"
