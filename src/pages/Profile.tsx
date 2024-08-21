efe430adb68e02aa5bcb297481ff07ee283019dc
Calvin <calvinalvito19@gmail.com>
2024-08-21 15:53:51 +0700

    feature: add profile page and context

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,16 +1,60 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import InfoUser from "../components/layout/infouser/InfoUser";
 import MenuFitur from "../components/layout/menufitur/MenuFitur";
 import ProfileLayout from "../components/layout/profile";
+import { useLoading } from "../contexts/useLoading";
+import { useAuth } from "../contexts/useAuth";
+import useProfile from "../contexts/useProfile";
+import useBankStatement from "../contexts/useBankStatement";
+import Preloading from "../components/base/preloading/preloading";
 
 const Profile: React.FC = () => {
+  const { loading, setLoading } = useLoading();
+  const { fetchLoginInfo } = useAuth();
+  const { fetchProfile } = useProfile();
+  const { fetchBankStatement } = useBankStatement();
+
+  const [hasFetchedData, setHasFetchedData] = useState(false);
+
+  useEffect(() => {
+    if (!hasFetchedData) {
+      const fetchData = async () => {
+        setLoading(true);
+        try {
+          await Promise.all([
+            fetchLoginInfo(),
+            fetchBankStatement(),
+            fetchProfile(),
+          ]);
+          setHasFetchedData(true);
+        } catch (err) {
+          console.error("Error fetching data", err);
+        } finally {
+          setLoading(false);
+        }
+      };
+
+      fetchData();
+    }
+  }, [
+    hasFetchedData,
+    fetchLoginInfo,
+    fetchBankStatement,
+    fetchProfile,
+    setLoading,
+  ]);
+
   return (
     <div className="font-jakartasans bg-fill0">
-      <>
-        <InfoUser />
-        <MenuFitur />
-        <ProfileLayout/>
-      </>
+      {loading ? (
+        <Preloading />
+      ) : (
+        <>
+          <InfoUser />
+          <MenuFitur />
+          <ProfileLayout />
+        </>
+      )}
     </div>
   );
 };
