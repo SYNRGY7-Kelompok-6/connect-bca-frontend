20be2ffc436960ae44caf77b7784fe1322fab948
Calvin <calvinalvito19@gmail.com>
2024-08-24 13:55:02 +0700

    fix: fix qris transfer for bug

--- src/components/layout/qristransfer/QrisTransfer.tsx
+++ src/components/layout/qristransfer/QrisTransfer.tsx
@@ -1,52 +1,39 @@
 import React, { useState, useEffect, useCallback, useRef } from "react";
+import useQrisTransfer from "../../../../src/contexts/useQrisTransfer";
 import useBankStatement from "../../../contexts/useBankStatement";
-import useQrisTransfer from '../../../../src/contexts/useQrisTransfer';
+import { useAuth } from "../../../contexts/useAuth";
 import html2canvas from "html2canvas";
-import QrisModal from './qrismodal';
-import QrisInfo from './qrisinfo';
-import QrisImage from './qrisimage';
-import QrisButton from './qrisbutton';
+import QrisInfo from "./qrisinfo";
+import QrisButton from "./qrisbutton";
+import QrisImage from "./qrisimage";
 import Skeleton from "../../base/skeletonloading";
 import logoQris from "../../../../public/logo qris.svg";
-import { formatToIDR, formatNumber } from '../../hooks/formatRp';
-import { useTimeout } from '../../hooks/changeToTime';
+import { useTimeout } from "../../hooks/changeToTime";
 
 const QrisTransfer: React.FC = () => {
   const [loading, setLoading] = useState(true);
-  const [modalOpen, setModalOpen] = useState(false);
   const [expiresTime, setExpiresTime] = useState(false);
-  const [price, setPrice] = useState<number>(0);
-  const [nominal, setNominal] = useState<string>("");
-  const [buttonText, setButtonText] = useState("Tambah Detail QRIS");
-  const [buttonExpiredInfo, setButtonExpiredInfo] = useState(false)
+  const [buttonExpiredInfo, setButtonExpiredInfo] = useState(false);
   const { bankStatement, fetchBankStatement } = useBankStatement();
+  const { fetchLoginInfo } = useAuth();
   const { qrImage, generateQRIS, expiresAt } = useQrisTransfer();
   const timeLeft = useTimeout(expiresAt);
-  const qrisTransferRef = useRef(null);
+  const qrisTransferRef = useRef<HTMLDivElement | null>(null);
+  const fetchQrisTransferRef = useRef(false);
 
   const fetchQrisTransfer = useCallback(async () => {
     try {
-      await generateQRIS(price, "IDR");
+      await generateQRIS();
     } catch (err) {
-      console.error('Error generating QRIS:', err);
+      console.error("Error generating QRIS:", err);
     }
-  }, [generateQRIS, price]);
-
-  const handleQrisTransfer = async () => {
-    setButtonText(formatToIDR(price));
-    setButtonExpiredInfo(true);
-    setModalOpen(false);
-    setExpiresTime(true);
-    await fetchQrisTransfer();
-  };
+  }, [generateQRIS]);
 
   const handleRefresh = async () => {
-    setNominal("");
-    setButtonText("Tambah Detail QRIS");
     setButtonExpiredInfo(false);
     await fetchQrisTransfer();
     setExpiresTime(false);
-  };
+  }
 
   const handleDownload = async () => {
     if (!qrisTransferRef.current) return;
@@ -54,12 +41,11 @@ const QrisTransfer: React.FC = () => {
     try {
       const canvas = await html2canvas(qrisTransferRef.current, {
         useCORS: true,
-        scale: 2, 
+        scale: 2,
       });
 
-      const resizedCanvas = document.createElement('canvas');
-      const ctx = resizedCanvas.getContext('2d');
-
+      const resizedCanvas = document.createElement("canvas");
+      const ctx = resizedCanvas.getContext("2d");
       const desiredWidth = 450;
       const desiredHeight = 600;
 
@@ -70,25 +56,28 @@ const QrisTransfer: React.FC = () => {
         ctx.drawImage(canvas, 0, 0, desiredWidth, desiredHeight);
       }
 
-      const imgData = resizedCanvas.toDataURL('image/png');
-
-      const link = document.createElement('a');
+      const imgData = resizedCanvas.toDataURL("image/png");
+      const link = document.createElement("a");
       link.href = imgData;
-      link.download = 'qris-image.png'; 
+      link.download = "qris-image.png";
 
       document.body.appendChild(link);
       link.click();
-
       document.body.removeChild(link);
     } catch (error) {
-      console.error('Error generating PNG:', error);
+      console.error("Error generating PNG:", error);
     }
   };
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        await Promise.all([fetchBankStatement()]);
+        if (!fetchQrisTransferRef.current) {
+          await fetchQrisTransfer();
+          fetchQrisTransferRef.current = true;
+        }
+        await fetchBankStatement();
+        await fetchLoginInfo();
       } catch (err) {
         console.error("Error fetching data", err);
       } finally {
@@ -97,74 +86,51 @@ const QrisTransfer: React.FC = () => {
     };
 
     fetchData();
-  }, [fetchBankStatement, fetchQrisTransfer]);
+  }, [fetchQrisTransfer, fetchBankStatement, fetchLoginInfo]);
 
   useEffect(() => {
-    console.log("Expires At:", expiresAt);
-    console.log("Time Left:", timeLeft);
-  }, [expiresAt, timeLeft]);
-
-  const handleOpenModal = () => setModalOpen(true);
-  const handleCloseModal = () => setModalOpen(false);
-
-  const handleNominalChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { value } = e.target;
-    const cleanedValue = value.replace(/[^0-9]/g, "");
-    if (cleanedValue === "") {
-      setNominal("");
-      setPrice(0);
+    if (timeLeft === "Expired") {
+      setExpiresTime(true);
     } else {
-      const numberValue = parseFloat(cleanedValue);
-
-      if (!isNaN(numberValue)) {
-        setPrice(numberValue);
-        setNominal(formatNumber(numberValue));
-      } else {
-        setNominal(cleanedValue);
-        setPrice(0);
-      }
+      setExpiresTime(false);
     }
-  };
+  }, [timeLeft]);
 
   return (
-    <>
-      <div className="bg-neutran-1 shadow-box rounded flex flex-col md:w-96 w-full p-5 gap-2.5 mt-16 md:mt-0"
-        ref={qrisTransferRef} >
-        <div className="flex justify-between items-center h-[55px] border-b"
-          style={{
-            borderColor: '#0a3967'
-          }}>
-          <img src={logoQris} alt="logoQris" className="h-[24px]" />
-        </div>
-        <div className="flex flex-col justify-center text-center items-center">
-          {loading ? (
-            <Skeleton className="h-6 w-48" />
-          ) : (
-            <>
-              <QrisInfo bankStatement={bankStatement || undefined} />
-              <QrisImage qrImage={qrImage || undefined} expiresTime={expiresTime} />
-              <QrisButton
-                buttonText={buttonText}
-                handleOpenModal={handleOpenModal}
-                handleRefresh={handleRefresh}
-                handleDownlaod={handleDownload}
-                timeLeft={buttonExpiredInfo ? `Masa Berlaku : ${timeLeft}` : "QR berlaku untuk 1 kali transaksi"}
-              />
-            </>
-          )}
-        </div>
+    <div
+      className="bg-neutran-1 shadow-box rounded flex flex-col md:w-96 w-full p-5 gap-2.5 mt-16 md:mt-0"
+      ref={qrisTransferRef}
+    >
+      <div
+        className="flex justify-between items-center h-[55px] border-b"
+        style={{ borderColor: "#0a3967" }}
+      >
+        <img src={logoQris} alt="logoQris" className="h-[24px]" />
       </div>
-      <div>
-        {modalOpen && (
-          <QrisModal
-            handleCloseModal={handleCloseModal}
-            handleQrisTransfer={handleQrisTransfer}
-            nominal={nominal}
-            handleNominalChange={handleNominalChange}
-          />
+      <div className="flex flex-col justify-center text-center items-center">
+        {loading ? (
+          <Skeleton className="h-6 w-48" />
+        ) : (
+          <>
+            <QrisInfo bankStatement={bankStatement || undefined} />
+            <QrisImage
+              qrImage={qrImage || undefined}
+              expiresTime={expiresTime}
+              timeLeft={timeLeft}
+            />
+            <QrisButton
+              handleRefresh={handleRefresh}
+              handleDownload={handleDownload}
+              timeLeft={
+                buttonExpiredInfo
+                  ? `Masa Berlaku : ${timeLeft}`
+                  : "QR berlaku untuk 1 kali transaksi"
+              }
+            />
+          </>
         )}
       </div>
-    </>
+    </div>
   );
 };
 
--- src/components/layout/qristransfer/qrisimage/QrisImage.tsx
+++ src/components/layout/qristransfer/qrisimage/QrisImage.tsx
@@ -1,32 +1,43 @@
-import React from 'react';
+import React from "react";
 
 interface QrisImageProps {
   qrImage?: string;
   expiresTime: boolean;
+  timeLeft: string;
 }
 
-const QrisImage: React.FC<QrisImageProps> = ({ qrImage, expiresTime }) => (
-  <>
-    {expiresTime ? (
-      <img
-        src={qrImage}
-        className="mt-[28px]"
-        style={{ width: '150px', height: '150px' }}
-        alt="logoQrisTransfer"
-        aria-label="Qris Transfer"
-      />
-    ) : (
-      <div
-        className="mt-[28px] text-primary-dark-blue text-base"
-        style={{ width: '150px', height: '150px', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', border: '1px solid #ccc' }}
-      >
-        <div className='w-[100px]'
-          aria-label="Silahkan Masukan Nominal">
-          Silahkan Masukan Nominal
+const QrisImage: React.FC<QrisImageProps> = ({
+  qrImage,
+  expiresTime,
+  timeLeft,
+}) => {
+  return (
+    <div className="relative flex flex-col justify-center items-center">
+      {expiresTime && (
+        <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center bg-gray-800 bg-opacity-50">
+          <span className="text-white text-lg">QR Code Telah Kedaluwarsa</span>
+        </div>
+      )}
+      <div className="flex flex-col items-center">
+        {qrImage ? (
+          <img
+            src={qrImage}
+            alt="QRIS"
+            className={`w-auto h-auto ${
+              expiresTime ? "opacity-50" : "opacity-100"
+            }`}
+          />
+        ) : (
+          <div className="w-32 h-32 border border-dashed border-gray-400 flex items-center justify-center">
+            <span>QR Code Tidak Tersedia</span>
+          </div>
+        )}
+        <div className="mt-2 text-base text-neutral-9">
+          {expiresTime ? `QR Code kadaluarsa` : `Sisa waktu: ${timeLeft}`}
         </div>
       </div>
-    )}
-  </>
-);
+    </div>
+  );
+};
 
 export default QrisImage;
