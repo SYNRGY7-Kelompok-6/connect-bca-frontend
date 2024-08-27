368ed5d873ca406be95365d896c7b9b601fc7da0
Calvin <calvinalvito19@gmail.com>
2024-08-27 12:53:58 +0700

    feature: add auto logout in private routes

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../components/layout/header";
 import FormLogin from "../components/layout/formlogin";
 import Carousel from "../components/layout/carousel";
 import Information from "../components/layout/information";
+import { useLocation } from "react-router-dom";
+import Popup from "../components/base/popup";
 
 const Login: React.FC = () => {
+  const location = useLocation();
+  const [isPopupVisible, setPopupVisible] = useState(
+    location.state?.showPopup || false
+  );
+
+  const handleClosePopup = () => {
+    setPopupVisible(false);
+  };
+
   return (
-    <body className="bg-fill0 font-jakartasans">
+    <div className="bg-fill0 font-jakartasans">
       <Header />
       <div className="container mx-auto lg:mt-14 md:mt-5 flex flex-col gap-5 px-4">
         <div className="flex lg:flex-row flex-col lg:gap-[52px] gap-10">
@@ -15,7 +26,17 @@ const Login: React.FC = () => {
         </div>
         <Information />
       </div>
-    </body>
+      {isPopupVisible && (
+        <Popup
+          message="Sesi Anda telah berakhir. Silakan login kembali."
+          svgSrc="/AlertError.svg"
+          svgAlt="Alert Error"
+          buttonText="Tutup"
+          onButtonClick={handleClosePopup}
+          labelButton="Tutup"
+        />
+      )}
+    </div>
   );
 };
 
--- src/routes/PrivateRoutes.tsx
+++ src/routes/PrivateRoutes.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Navigate, useLocation } from "react-router-dom";
+import React, { useCallback, useEffect, useRef } from "react";
+import { Navigate, useLocation, useNavigate } from "react-router-dom";
 import { useAuth } from "../contexts/useAuth";
 
 interface PrivateRouteProps {
@@ -7,14 +7,49 @@ interface PrivateRouteProps {
 }
 
 const PrivateRoute: React.FC<PrivateRouteProps> = ({ children }) => {
-  const { accessToken } = useAuth();
+  const { accessToken, logout } = useAuth();
+  const navigate = useNavigate();
   const location = useLocation();
+  const timeoutRef = useRef<ReturnType<typeof setTimeout>>();
 
-  return accessToken ? (
-    children
-  ) : (
-    <Navigate to="/login" state={{ from: location }} replace />
-  );
+  const resetTimeout = useCallback(() => {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+
+    timeoutRef.current = setTimeout(() => {
+      logout();
+      navigate("/login", { state: { showPopup: true } });
+    }, 300000);
+  }, [logout, navigate]);
+
+  useEffect(() => {
+    const events = ["mousemove", "mousedown", "keypress", "touchstart"];
+    const eventListener = () => resetTimeout();
+    events.forEach((event) => window.addEventListener(event, eventListener));
+    resetTimeout();
+
+    return () => {
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+      }
+      events.forEach((event) =>
+        window.removeEventListener(event, eventListener)
+      );
+    };
+  }, [resetTimeout]);
+
+  if (!accessToken) {
+    return (
+      <Navigate
+        to="/login"
+        state={{ from: location, showPopup: true }}
+        replace
+      />
+    );
+  }
+
+  return children;
 };
 
 export default PrivateRoute;
